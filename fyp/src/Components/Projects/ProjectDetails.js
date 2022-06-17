53301e9b4ca68abba104efaa70eb87d38ed80c36
Mirza Ahmer Gull <mirzaahmergull@outlook.com>
2022-06-17 11:03:37 +0500

    Integrated project detaails and request details page

--- fyp/src/Components/Projects/ProjectDetails.js
+++ fyp/src/Components/Projects/ProjectDetails.js
@@ -1,67 +1,91 @@
 import React, { useEffect, useState } from "react";
-import './project.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./project.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function ProjectDetails(props){
- const[project,setProject]=useState(JSON.parse(props.match.params.object))
- 
-        return(
-            <div>
-            
-                <div className='divMain'> 
-                <br></br>
-                <table className="table" width="90%" cellPadding="10%" border="0">
-                    <tr>
-                        <td >
+function ProjectDetails(props) {
+  const [projectDetails, setProjectDetails] = useState(null);
+  useEffect(()=>{
+    let current_project_id= window.location.href.split("/")[window.location.href.split("/").length-1]
+    console.log("CURRENT PROJECT ID",current_project_id)
+    fetch("/supervisor/project-details/",{
+        method:"POST",
+        headers:{"Content":"application/json"},
+        body :JSON.stringify({"id":current_project_id}),
+      }
+      ).then(data=>data.json()).then((data)=>{
+        console.log(data);
+        setProjectDetails(data);
+      })
+      return null;
+  },[])
+
+  return (
+    <div>
+      <div className="divMain">
+        <br></br>
+        <table className="table" width="90%" cellPadding="10%" border="0">
+          <tr>
+            <td>
+              <div>
+                <h4 className="heading">
+                  Project ID:{" "}
+                  <font class="blockquote">{(projectDetails ? projectDetails.id : "")}</font>{" "}
+                </h4>
+              </div>
+              <hr className="hr"></hr>
+              <div>
+                <h4 className="heading">
+                  Project Title: <font class="blockquote">{(projectDetails ? projectDetails.title : "")}</font>{" "}
+                </h4>
+              </div>
+              <hr className="hr"></hr>
+              <div>
+                <h4 className="heading">
+                  Team Leader: <font class="blockquote">{(projectDetails ? projectDetails.team_members[0].name : "")}</font>
+                </h4>
+              </div>
+              <hr className="hr"></hr>
+              <div>
+                <h4 className="heading">
+                  Team Members:
+                  <font className="blockquote">
+                    {( projectDetails ? projectDetails.team_members.map((member) => {
+                      return (
                         <div>
-                            
-                               <h4 className='heading'>Project ID: <font class="blockquote">{project.projectId}</font> </h4> 
-                            </div>
-                            <hr className="hr"></hr>
-                            <div>
-                               <h4 className='heading'>Project Title: <font class="blockquote">{project.title}</font> </h4> 
-                            </div>
-                            <hr className="hr"></hr>
-                            <div>
-                                <h4 className='heading'>
-                                    Team Leader: <font class="blockquote">{project.leader}</font>
-                                </h4>
-                                
-                            </div>
-                            <hr className="hr"></hr>
-                            <div><h4 className='heading'>
-                                Team Members:
-                                 <font className="blockquote">
-                                    {project.members.map((member)=>{
-                                         return (
-                                        <div><li>{member}</li></div>
-                                     ); })}
-                                </font>
-                            </h4>
-                            </div>
-                            
-                            <div><h4 className='heading'>
-                                Team Email Ids:
-                                 <font className="blockquote">
-                                    {project.emailIds.map((email)=>{
-                                         return (
-                                        <div><li>{email}</li></div>
-                                     ); })}
-                                </font>
-                            </h4>
-                            </div>
-                            <hr className="hr"></hr>
-                            <div>
-                                <h4 className='heading'>Project Status:  <font class="blockquote">{project.status}</font>
-                                    </h4>
-                                   
-                            </div>
-                        </td>
-                    </tr>
-                </table>
-            
-                </div>
-                </div>
-        );
+                          <li>{member.name}</li>
+                        </div>
+                      );
+                    }) : "")}
+                  </font>
+                </h4>
+              </div>
+
+              <div>
+                <h4 className="heading">
+                  Team Email Ids:
+                  <font className="blockquote">
+                    {(projectDetails ? projectDetails.team_members.map((member) => {
+                      return (
+                        <div>
+                          <li>{member.email}</li>
+                        </div>
+                      );
+                    }) : "")}
+                  </font>
+                </h4>
+              </div>
+              <hr className="hr"></hr>
+              <div>
+                <h4 className="heading">
+                  Project Status:{" "}
+                  <font class="blockquote">{(projectDetails ? projectDetails.status : "")}</font>
+                </h4>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
 export default ProjectDetails;
--- fyp/src/Components/Supervisor/Requests/ViewRequest.js
+++ fyp/src/Components/Supervisor/Requests/ViewRequest.js
@@ -1,51 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./requests.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 function ViewRequest(props) {
-    const[request,setRequest]=useState(JSON.parse(props.match.params.request))  
-return(
-<div>
-<div className="topBar">
-          <p className="dashboard"><b>
-            Request Form
-            </b>
-          </p>
+  const [request, setRequest] = useState(null);
+
+  useEffect(()=>{
+    let current_request_id= window.location.href.split("/")[window.location.href.split("/").length-1]
+    console.log("CURRENT PROJECT ID",current_request_id)
+    fetch("/supervisor/request-details",{
+        method:"POST",
+        headers:{"Content":"application/json"},
+        body :JSON.stringify({"id":current_request_id}),
+      }
+      ).then(data=>data.json()).then((data)=>{
+        console.log(data);
+        setRequest(data);
+      })
+      return null;
+  },[])
+
+
+  return (
+    <div>
+      <div className="topBar">
+        <p className="dashboard">
+          <b>Request Details</b>
+        </p>
+      </div>
+      <div className="divStyle">
+        <form>
+          <div className="row pt-5 mx-auto">
+            <h3 className="data">Title:</h3>
+            <div className="col-8 form-group mx-auto">
+              <div class="form-control">{(request ? request.title : "")}</div>
+            </div>
+            <h3 className="data">Email IDs:</h3>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <div class="form-control"><text>{(request ? request.email : "")}</text></div>
+            </div>
+            <h3 className="data">Content:</h3>
+            <div className="col-8 form-group pt-2 mx-auto">
+              <div class="form-control">{(request ? request.description : "")}</div>
+            </div>
           </div>
-<div className="divStyle">
-<form>
-<div className="row pt-5 mx-auto">
-    <h3 className="data">Title:</h3>
-<div className="col-8 form-group mx-auto">
-<div class="form-control">
-    {request.title}
+        </form>
+      </div>
     </div>
-</div>
-<h3 className="data">Email ID:</h3>
-<div className="col-8 form-group pt-2 mx-auto">
-<div class="form-control">
-    {request.email}
-    </div>
-</div>
-<h3 className="data">Name:</h3>
-<div className="col-8 form-group pt-2 mx-auto">   
-<div class="form-control">
-    {request.sender}
-    </div>
-</div>
-<h3 className="data">Content:</h3>
-<div className="col-8 form-group pt-2 mx-auto">
-<div class="form-control">
-    {request.detail}
-    </div>
-</div>
-
-
-
-</div>
-</form>
-</div>
-</div>
-);
+  );
 }
 
 export default ViewRequest;
